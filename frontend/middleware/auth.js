eedca1d91e75cb1bb5e58dc535081f2b52ebfcef
Timomaugu <timomaugu@gmail.com>
2025-02-27 17:27:23 +0300

    fixed errors

--- frontend/middleware/auth.js
+++ frontend/middleware/auth.js
@@ -1,7 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     const token = localStorage.getItem("token");
     if ( typeof(token) !== "undefined" && token !== null ) {
-      isLoggedIn = true;
       return navigateTo('/');
     }
     else {
